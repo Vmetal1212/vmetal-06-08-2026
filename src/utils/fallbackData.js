442d9089b96e53236a48bc1751853f43dd199f08
V Metal Solutions INC <vinal@vmetalsolutions.com>
2026-08-19 12:52:15 +0530

    Create fallbackData.js

---
+++ src/utils/fallbackData.js
@@ -0,0 +1,102 @@
+export const FALLBACK_PRODUCTS = [
+  {
+    id: 1,
+    attributes: {
+      name: "Hot-Rolled (HR) Steel",
+      slug: "hr-hrpo",
+      image: { data: [{ attributes: { url: "/images/product1.png" } }] }
+    }
+  },
+  {
+    id: 2,
+    attributes: {
+      name: "Cold-Rolled (CR) Steel",
+      slug: "cr-crca",
+      image: { data: [{ attributes: { url: "/images/ourProducts1.jpg" } }] }
+    }
+  },
+  {
+    id: 3,
+    attributes: {
+      name: "Galvanized Iron (GI)",
+      slug: "gi",
+      image: { data: [{ attributes: { url: "/images/ourProducts.jpg" } }] }
+    }
+  },
+  {
+    id: 4,
+    attributes: {
+      name: "PPGI/PPGL",
+      slug: "ppgi",
+      coverImage: { data: { attributes: { url: "/images/asset2.avif" } } },
+      image: { data: [{ attributes: { url: "/images/asset2.avif" } }] }
+    }
+  },
+  {
+    id: 5,
+    attributes: {
+      name: "PMP Plates",
+      slug: "pmp",
+      image: { data: [{ attributes: { url: "/images/services.jpg" } }] }
+    }
+  },
+  {
+    id: 6,
+    attributes: {
+      name: "MS Structure",
+      slug: "ms-structure",
+      image: { data: [{ attributes: { url: "/images/services1.jpg" } }] }
+    }
+  },
+  {
+    id: 7,
+    attributes: {
+      name: "MS Pipes",
+      slug: "ms-pipes",
+      image: { data: [{ attributes: { url: "/images/Home1.jpg" } }] }
+    }
+  },
+  {
+    id: 8,
+    attributes: {
+      name: "Z/C Purlin",
+      slug: "zc-purlin",
+      image: { data: [{ attributes: { url: "/images/ourProducts.jpg" } }] }
+    }
+  }
+];
+
+export const FALLBACK_SERVICES = [
+  {
+    id: 1,
+    attributes: {
+      name: "Cut-To-Length (CTL)",
+      slug: "cut-to-length",
+      image: { data: [{ attributes: { url: "/images/services1.jpg" } }] }
+    }
+  },
+  {
+    id: 2,
+    attributes: {
+      name: "Slitting Service",
+      slug: "slitting",
+      image: { data: [{ attributes: { url: "/images/ourProducts.jpg" } }] }
+    }
+  },
+  {
+    id: 3,
+    attributes: {
+      name: "Corrugation & Profiling",
+      slug: "corrugation-profiling",
+      image: { data: [{ attributes: { url: "/images/Home1.jpg" } }] }
+    }
+  },
+  {
+    id: 4,
+    attributes: {
+      name: "C/Z Purlin",
+      slug: "zc-purlin",
+      image: { data: [{ attributes: { url: "/images/services.jpg" } }] }
+    }
+  }
+];
