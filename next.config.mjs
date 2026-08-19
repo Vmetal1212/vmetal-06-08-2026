08c79b4b79ac52919b44a05a33eed31e6817f4e7
V Metal Solutions INC <vinal@vmetalsolutions.com>
2026-08-19 12:40:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,9 @@ const nextConfig = {
     ],
   },
   env: {
-    API_URL: process.env.API_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    WEB_URL: process.env.WEB_URL,
+    API_URL: process.env.API_URL || "https://www.vmetalsolutions.com",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://www.vmetalsolutions.com/api",
+    WEB_URL: process.env.WEB_URL || "https://www.vmetalsolutions.com",
   },
   trailingSlash: true,
 };
